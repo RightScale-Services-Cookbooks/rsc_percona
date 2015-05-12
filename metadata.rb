7deb2e9192e31f986bc37d0d6658ef02eb4a06ca
Curt Wilhelm <curt@rightscale.com>
2015-05-12 09:51:55 -0600

    fix foodcritic errors

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Installs and configures a Percona server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.6'
+version          '1.0.13'
 
 depends 'rs-mysql', '1.1.6'
 depends 'apt'
